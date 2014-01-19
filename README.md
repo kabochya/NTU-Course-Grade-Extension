398df52c5bfc18548a17e3d23b1ec4842e35352f
kabochya <pml0415@gmail.com>
2014-01-19 16:57:35 +0800

    fixed bugs and UI
    reverse grade seq
    no multiclicks
    text newline

--- README.md
+++ README.md
@@ -4,14 +4,13 @@ NTU-Course-Grade-Extension
 1.抓下來
 2.把資料夾拉出來
 3.打開chrome
-4.在URL列打chrome://extension
+4.在URL列打chrome://extensions
 (4.5)打開開發者模式
 5.把剛剛的資料夾拉進畫面
 6.到選課網 要看分布直接按左邊"成績分布"...Done
 
 KNOWN BUGS
-1.連點(應該秒殺吧XD)
-2.字塞不下
+1.字塞不下
 
 TODO
 1.UI optimization
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var active_frame=window.parent.frames[2].document;
 var page_top=window.top.document;
-var grades=['X/F','C-','C ','C+','B-','B ','B+','A-','A ','A+'];
-var grades_css=['F','C-','C ','Cp','B-','B ','Bp','A-','A ','Ap'];
+var grades=['X,F','C-','C ','C+','B-','B ','B+','A-','A ','A+'];
+var grades_css=['F','C-','C','Cp','B-','B','Bp','A-','A','Ap'];
 $('tr[align="center"]',active_frame).each(function(i){
-	$(this).prepend('<td '+(i==0?'':'class="grade"')+'>成績分布</td>');
+	$(this).prepend('<td '+(i==0?'':'class="grade_btn" active=0')+'>成績分布</td>');
+	$('.grade_btn').width('60px');
 	$(this).after('<tr class="grade_row"><td class="grade_box" colspan="17"><div class="bar"></div><div class="text"></div></td></tr>');
 });
 $('tr.grade_row',active_frame).hide();
@@ -11,14 +12,18 @@ $('tr.grade_row',active_frame).hide();
 if(window.parent.frames[2]==window){
 	$('<link rel="stylesheet" type="text/css" href='+chrome.extension.getURL('style.css')+'>').appendTo('html');
 }
-$(".grade",active_frame).click(function(){
+$(".grade_btn",active_frame).click(function(){
 	var $this=$(this);
+	if($this.attr('active')==1){
+		return false;
+	}
 	var $current_row=$this.parent('tr').next('tr.grade_row');
 	var $current_box=$current_row.children('td');
 	var year=$('#select_sem option:eq(0)',active_frame).val().split('-')[0]-1;
 	var term=$('#select_sem option[selected=""]',active_frame).val().split('-')[1];
 	var course_code=$this.siblings('td:eq(6)').text();
 	var class_id=$this.siblings('td:eq(3)').text();
+	$this.attr('active',1);
 	$.get("//if163.aca.ntu.edu.tw/eportfolio/student/Curve.asp?Year="+year+"&Term="+term+"&CouCode="+course_code+"&Class="+(class_id.length==2?class_id:''),
 		function(res){
 			var grade_arr = res.match(/>\(\d+/g);
@@ -28,7 +33,7 @@ $(".grade",active_frame).click(function(){
 				for(var i=0;i<grade_arr.length;i++){
 					grade_arr[i]=parseInt(grade_arr[i].replace(/>\(/g,''));
 					accum[i]=(i==0?0:accum[i-1])+grade_arr[i];
-					$current_box.children('.bar').prepend('<span class="grade_bar '+grades_css[i]+'"><span class="bar_desc">'+grades[i]+':'+grade_arr[i]+'</span></span>');
+					$current_box.children('.bar').prepend('<span class="grade_bar '+grades_css[i]+'"><span class="bar_desc">'+grades[i]+'<br>'+grade_arr[i]+'</span></span>');
 				}
 				for(var i=0;i<grade_arr.length;i++){
 					if(grade_arr[i]!=0){
